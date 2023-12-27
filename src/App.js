6ef44ec035f0280be3f7f142b83433339081d339
thomyhabesha <thomastafese3337@gmail.com>
2023-12-27 07:41:44 +0300

    'forth'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from './pages/Home';
-import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import {HashRouter, Routes,Route} from 'react-router-dom'
 import Apply from './pages/apply/Apply';
 import Info from './pages/information/Info';
 import Terms from './pages/terms/Terms';
@@ -9,16 +9,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>    
-        <Route path="/apply" element={<Apply/>} />
-        <Route path="/info" element={<Info/>} />
-        <Route path="/terms" element={<Terms/>} />
-        <Route path="/privacy" element={<Privacy/>} />
-              
-          </Routes>
-      </BrowserRouter>
+       <HashRouter>
+<Routes>
+
+<Route exact path="/" element={<Home/>}/>
+    <Route path="/apply" element={<Apply/>}/>
+    <Route path="/info" element={<Info/>}/>
+    <Route path="/terms" element={<Terms/>}/>
+    <Route path="/privacy" element={<Privacy/>}/>
+         
+</Routes>
+</HashRouter>
      
     </div>
   );
